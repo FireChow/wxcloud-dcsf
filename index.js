085a13ecaf25a346b5b06f334d72e3031db7369a
Fire Chow <465274070@qq.com>
2022-07-11 13:29:04 +0800

    update js

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ const logger = require("koa-logger");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
-const { init: initDB, Counter } = require("./db");
+const { init: initDB, User } = require("./db");
 
 const router = new Router();
 
 const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
+const txtPage = fs.readFileSync(path.join(__dirname, "MP_verify_4PHUAKsyG7gnjDcD.txt"), "utf-8");
 
 // 首页
 router.get("/", async (ctx) => {
   ctx.body = homePage;
 });
 
+router.get("/MP_verify_4PHUAKsyG7gnjDcD.txt", async (ctx) => {
+  ctx.body = txtPage;
+});
+
 // 更新计数
-router.post("/api/count", async (ctx) => {
+router.post("/api/user", async (ctx) => {
   const { request } = ctx;
-  const { action } = request.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
+  const { user } = request.body;
+  let newUser = User.build(user)
+  let result = await newUser.save()
 
   ctx.body = {
-    code: 0,
-    data: await Counter.count(),
+    code: 200,
+    data: result
   };
 });
 
 // 获取计数
-router.get("/api/count", async (ctx) => {
-  const result = await Counter.count();
-
+router.get("/api/user/:openid", async (ctx) => {
+  const { openid } = ctx.params;
+  let user = await User.findOne({ where: { openid } })
   ctx.body = {
-    code: 0,
-    data: result,
+    code: 200,
+    data: user
   };
 });
 
@@ -55,7 +55,7 @@ app
   .use(logger())
   .use(bodyParser())
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
 
 const port = process.env.PORT || 80;
 async function bootstrap() {
